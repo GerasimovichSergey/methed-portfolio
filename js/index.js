2cbbf382656b0f5deeab33614a3265cb4eaff087
Gerasimovich Sergey <wmmoee@gmail.com>
2021-11-17 00:40:56 +0300

    feat: Gallary

---
+++ js/index.js
@@ -0,0 +1,150 @@
+const disabledScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+    document.body.scrollPosition = window.scrollY;
+
+    document.documentElement.style.cssText = `
+        position: relative;
+        height: 100vh;
+    `;
+
+    document.body.style.cssText = `
+        overflow: hidden;
+        position: fixed;
+        top: -${document.body.scrollPosition}px;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+        padding-right: ${widthScroll}px; 
+    `;
+};
+
+const enabledScroll = () => {
+    document.documentElement.style.cssText = '';
+    document.body.style.cssText = 'position: relative;';
+    window.scroll({top: document.body.scrollPosition});
+};
+
+{   // Модальное окно
+    const presentOrderBtn = document.querySelector('.present__order-btn');
+    const pageOverlayModal = document.querySelector('.page__overlay_modal');
+    const modalClose = document.querySelector('.modal__close');
+
+    const handlerModal = (openBtn, modal, openSelector, closeBtn, sk) => {
+
+        const speed = {
+            slow: 15,
+            medium: 8,
+            fast: 1,
+            default: 5,
+        };
+
+        let opacity = 0;
+
+        const openModal = () => {
+            disabledScroll();
+            modal.style.opacity = opacity;
+
+            modal.classList.add(openSelector);
+
+            const timer = setInterval(() => {
+                opacity += 0.02;
+                modal.style.opacity = opacity;
+
+                if (opacity >= 1) clearInterval(timer);
+            }, speed[sk] ? speed[sk] : speed.default);
+        };
+
+        const closeModal = () => {
+            enabledScroll();
+            const timer = setInterval(() => {
+                opacity -= 0.02;
+                modal.style.opacity = opacity;
+
+                if (opacity <= 0) {
+                    clearInterval(timer);
+                    modal.classList.remove(openSelector);
+                    opacity = 0;
+                    modal.style.opacity = 0;
+                }
+            }, speed[sk] ? speed[sk] : speed.default);
+        };
+
+        openBtn.addEventListener('click', openModal);
+
+        closeBtn.addEventListener('click', closeModal);
+
+        modal.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                closeModal();
+            }
+        })
+    };
+
+    handlerModal(presentOrderBtn, pageOverlayModal, 'page__overlay_modal_open', modalClose, 'fast');
+}
+
+{ // Бургер меню
+    const headerContacts = document.querySelector('.header__contacts');
+    const headerContactsBurger = document.querySelector('.header__contacts-burger');
+
+    const handlerBurger = (openBtn, menu, openSelector) => {
+        openBtn.addEventListener('click', () => {
+
+            if (menu.classList.contains(openSelector)) {
+                menu.style.height = '';
+                menu.classList.remove(openSelector);
+            } else {
+                menu.style.height = menu.scrollHeight + 'px';
+                menu.classList.add(openSelector);
+            }
+        });
+    };
+
+    handlerBurger(headerContactsBurger, headerContacts, 'header__contacts_open');
+}
+
+{ //Галерея
+    const portfolioList = document.querySelector('.portfolio__list');
+    const pageOverlay = document.createElement('div');
+
+    pageOverlay.classList.add('page__overlay');
+
+    portfolioList.addEventListener('click', (event) => {
+        disabledScroll();
+        const card = event.target.closest('.card');
+
+        if (card) {
+            document.body.append(pageOverlay);
+            const title = card.querySelector('.card__client');
+
+            const picture = document.createElement('picture');
+
+            picture.style.cssText = `
+               position: absolute;
+               top: 20px;
+               left: 50%;
+               transform: translateX(-50%);
+               width: 90%;
+               max-width: 1440px;
+            `;
+
+            picture.innerHTML = `
+                <source srcset="${card.dataset.fullImage}.avif" type="image/avif">
+                <source srcset="${card.dataset.fullImage}.webp" type="image/webp">
+                <img src="${card.dataset.fullImage}.jpg" alt="${title.textContent}">
+            `;
+
+            pageOverlay.append(picture);
+        }
+    });
+
+    pageOverlay.addEventListener('click', () => {
+        pageOverlay.remove();
+        pageOverlay.textContent = '';
+    });
+}
+
+{ // Работа с сервером
+
+}
